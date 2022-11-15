d8e2e949a44a200d8cc248260f21a9b49a7b67a6
Marcus V. Oliveira <marcusvoliveira@dcc.ufmg.br>
2022-11-15 12:09:37 -0300

    feat: add first version

---
+++ docker/sample_database/create_sample_db.sh
@@ -0,0 +1,2 @@
+/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P MyStrongPassword1234 -i create_objects.sql 
+/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P MyStrongPassword1234 -i load_data.sql
